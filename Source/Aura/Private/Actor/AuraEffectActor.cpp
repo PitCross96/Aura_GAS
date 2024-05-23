a51bbdcb2936e8cf424f7dbfe2c8c0aba085480a
Ultra <admin@ultra.site>
2024-05-23 11:14:19 +0200

    start hud

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -3,6 +3,8 @@
 
 #include "Actor/AuraEffectActor.h"
 #include "Components/SphereComponent.h"
+#include <AbilitySystemInterface.h>
+#include <AbilitySystem/AuraAttributeSet.h>
 
 AAuraEffectActor::AAuraEffectActor()
 {
@@ -18,7 +20,19 @@ AAuraEffectActor::AAuraEffectActor()
 void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OvverlappedComponent, AActor* OtherActor, 
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	//TODO: change to apply Gameplay Effect
+	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+		UAuraAttributeSet* MutableAurtaAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
+		MutableAurtaAttributeSet->SetHealth(MutableAurtaAttributeSet->GetHealth() + 25.f);
+		Destroy();
+	}
+}
 
+void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OvverlappedComponent, AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
 }
 
 void AAuraEffectActor::BeginPlay()
@@ -26,5 +40,6 @@ void AAuraEffectActor::BeginPlay()
 	Super::BeginPlay();
 	
 	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
 }
 
--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -16,7 +16,10 @@ class AURA_API AAuraEffectActor : public AActor
 public:	
 	AAuraEffectActor();
 
+	UFUNCTION()
 	virtual void OnOverlap(UPrimitiveComponent* OvverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	virtual void EndOverlap(UPrimitiveComponent* OvverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 protected:
 	virtual void BeginPlay() override;
